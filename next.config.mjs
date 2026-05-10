2e189ed38226c75f7bd786dba02c2c23efd3f5c7
DeveloperInPants <147731303+DeveloperInPants@users.noreply.github.com>
2026-05-10 16:59:41 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Критично для работы на Спринтхосте (Фри-1)
+  images: {
+    unoptimized: true, // Нужно для экспорта картинок
+  },
+};
 
+export default nextConfig;
