c618e1c2b6e6584e6597b7b202122a2077223bd7
DeveloperInPants <147731303+DeveloperInPants@users.noreply.github.com>
2026-05-10 16:58:45 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Tea Master HUB",
+  description: "Образовательная платформа для чайных мастеров",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
